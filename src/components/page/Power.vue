a79c1386d72567f995ac07798f762a5c0fc45001
kingLiYang <481767517@qq.com>
2018-05-10 18:42:27 +0800

    5-10

--- src/components/page/Power.vue
+++ src/components/page/Power.vue
@@ -2,102 +2,344 @@
     <div>
          <div class="divBut">
             <el-row>
-                <el-button type="primary" round>添加</el-button>
+                <el-button type="primary" round @click="add()">添加</el-button>
+
             </el-row>
+            <div style="margin:0 60px 0 0;">
+                总条数：<span>{{count}}</span>条
+            </div>
         </div>
 
         <!-- 表格 -->
          <table style="width:100%;border:1px solid #ccc;margin:10px 0 0 0;">
           <thead>
             <th>序号</th>
-            <th>权限名</th>
+            <th style="width:200px;padding:0 0 0 20px;">权限名</th>
             <th>权限</th>
             <th>是否为菜单</th>
             <th style="width:255px;">操作</th>
           </thead>
           <tbody>
             <template v-for="(item,index) in options">
-                <template v-if="item.son">
                     <tr>
-                        <td>{{index+1}}</td>
-                        <td>{{item.name}}</td>
+                        <td>{{Number(index)+1}}</td>
+                        <td>{{item.level}}{{item.name}}</td>
                         <td>{{item.controller}}/{{item.action}}</td>
                         <td v-if="item.type==1">是</td>
-                        <td v-if="item.type==0">否</td>
-                        <td>
-                            <el-button type="primary" size="small" round>添加子权限</el-button>
-                            <el-button type="primary" size="small" round>添加</el-button>
-                            <el-button type="primary" size="small" round>删除</el-button>
-                        </td>
-                    </tr>
-                    <template v-for="(ite,inde) in item.son">
-                        <template v-if="ite.son">
-                            <tr>
-                                <td>{{inde+1+index+1}}</td>
-                                <td>Ⅱ&nbsp;&nbsp; {{ite.name}}</td>
-                                <td>{{ite.controller}}/{{ite.action}}</td>
-                                <td v-if="ite.type==1">是</td>
-                                <td v-if="ite.type==0">否</td>
+                        <td v-if="item.type==0">否</td> 
+                            <template v-if="item.level!='------'">
                                 <td>
-                                    <el-button type="primary" size="small" round>添加子权限</el-button>
-                                    <el-button type="primary" size="small" round>添加</el-button>
-                                    <el-button type="primary" size="small" round>删除</el-button>
+                                    <el-button type="primary" size="small" round :data_id="item.p_path" @click="addChild($event)" >添加子权限</el-button>
+                                    <el-button type="warning" size="small" round :data_id="item.o_id" @click="editChild($event)">修改</el-button>
+                                    <el-button type="danger" size="small" round :data_id="item.p_path" @click="delChild($event)">删除</el-button>
                                 </td>
-                            </tr>
-                            <tr v-for="(item,ind) in ite.son">
-                                <td>{{inde+1+index+1+ind+1}}</td>
-                                <td>Ⅲ&nbsp;&nbsp; {{item.name}}</td>
-                                <td>{{item.controller}}/{{item.action}}</td>
-                                <td v-if="item.type==1">是</td>
-                                <td v-if="item.type==0">否</td>
+                            </template>
+                            <template v-else>
                                 <td>
-                                    <el-button type="primary" size="small" round>添加子权限</el-button>
-                                    <el-button type="primary" size="small" round>添加</el-button>
-                                    <el-button type="primary" size="small" round>删除</el-button>
+                                    <span style="width:90px;display:inline-block;"></span>
+                                    <el-button type="warning" size="small" round :data_id="item.o_id" @click="editChild($event)">修改</el-button>
+                                    <el-button type="danger" size="small" round :data_id="item.p_path" @click="delChild($event)">删除</el-button>
                                 </td>
-                            </tr>
-                        </template>
+                            </template>
+                            
                         
-                    </template>
-                    
-                </template>
+                    </tr>        
             </template>    
           </tbody>
         </table>
+        <!-- 添加子权限   弹框 -->
+        <el-dialog title="添加子权限" :visible.sync="dialogFormVisible">
+        <el-form :model="form">
+            <el-form-item label="权限名称" :label-width="formLabelWidth">
+            <el-input v-model="form.name" auto-complete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="权限类型" :label-width="formLabelWidth">
+            <el-select v-model="form.region" placeholder="请选择权限类型">
+                <el-option label="菜单权限" value="1"></el-option>
+                <el-option label="动作权限" value="0"></el-option>
+            </el-select>
+            </el-form-item>
+            <el-form-item label="控制器" :label-width="formLabelWidth">
+            <el-input v-model="form.con" auto-complete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="方法" :label-width="formLabelWidth">
+            <el-input v-model="form.act" auto-complete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="icon" :label-width="formLabelWidth">
+            <el-input v-model="form.icon" auto-complete="off"></el-input>
+            </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+            <el-button @click="dialogFormVisible = false">取 消</el-button>
+            <el-button type="primary" @click="addOrder()">确 定</el-button>
+        </div>
+        </el-dialog>
+                <!-- 修改权限   弹框 -->
+        <el-dialog title="修改权限" :visible.sync="dialogFormVisibleEdit">
+        <el-form :model="form">
+            <el-form-item label="权限名称" :label-width="formLabelWidth">
+            <el-input v-model="form.name" auto-complete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="权限类型" :label-width="formLabelWidth">
+            <el-select v-model="form.region" placeholder="请选择权限类型">
+                <el-option label="菜单权限" value="1"></el-option>
+                <el-option label="动作权限" value="0"></el-option>
+            </el-select>
+            </el-form-item>
+            <el-form-item label="控制器" :label-width="formLabelWidth">
+            <el-input v-model="form.con" auto-complete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="方法" :label-width="formLabelWidth">
+            <el-input v-model="form.act" auto-complete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="icon" :label-width="formLabelWidth">
+            <el-input v-model="form.icon" auto-complete="off"></el-input>
+            </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+            <el-button @click="dialogFormVisibleEdit = false">取 消</el-button>
+            <el-button type="primary" @click="editOrder()">确 定</el-button>
+        </div>
+        </el-dialog>
+        <!-- 删除   弹框 -->
+        <el-dialog
+            title="提示"
+            :visible.sync="dialogVisible"
+            width="30%">
+            <h3 style="text-align:center;">确定删除吗？</h3>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="dialogVisible = false">取 消</el-button>
+                <el-button type="primary" @click="delOrder()">确 定</el-button>
+            </span>
+            </el-dialog>
     </div>
 </template>
 <script>
-    export default {
-        data() {
-            return {
-                name: '',
-                options:[]
+export default {
+  data() {
+    return {
+      name: "",
+      options: [],
+      p_id: "",
+      count: "",
+      dialogFormVisible: false,
+      dialogVisible: false,
+      dialogFormVisibleEdit: false,
+      form: {
+        name: "",
+        region: "",
+        delivery: false,
+        con: "",
+        act: "",
+        icon: ""
+      },
+      formLabelWidth: "120px"
+    };
+  },
+  created() {
+    this.show(); // 组件刚渲染  获取数据
+  },
+  methods: {
+    show() {
+      // 组件刚渲染  获取数据
+      let that = this;
+      this.$axios({
+        url: "api/bqs/backend/web/index.php/oauth/list",
+        method: "get",
+        data: {},
+        transformRequest: [
+          function(data) {
+            let ret = "";
+            for (let it in data) {
+              ret +=
+                encodeURIComponent(it) +
+                "=" +
+                encodeURIComponent(data[it]) +
+                "&";
             }
+            return ret;
+          }
+        ],
+        headers: { "Content-Type": "application/x-www-form-urlencoded" }
+      }).then(function(res) {
+        that.options = res.data.data.data;
+        that.count = res.data.data.count;
+      });
+    },
+    add() {
+      // 添加 一级数据
+      this.p_id = 0;
+      this.dialogFormVisible = true;
+    },
+    addChild(e) {
+      // 添加子权限   弹框
+      this.p_id = e.currentTarget.getAttribute("data_id");
+      this.dialogFormVisible = true;
+    },
+    addOrder() {
+      // 添加子权限    提交
+      let that = this;
+      this.$axios({
+        url: "api/bqs/backend/web/index.php/oauth/add",
+        method: "post",
+        data: {
+          action: this.form.act,
+          controller: this.form.con,
+          name: this.form.name,
+          p_path: this.p_id,
+          type: this.form.region
         },
-        created(){
-                let that = this;
-                this.$axios({
-                url: "api/bqs/backend/web/index.php/oauth/list",
-                method: "get",
-                data: {},
-                transformRequest: [
-                    function(data) {
-                    let ret = "";
-                    for (let it in data) {
-                        ret +=
-                        encodeURIComponent(it) + "=" + encodeURIComponent(data[it]) + "&";
-                    }
-                    return ret;
-                    }
-                ],
-                headers: { "Content-Type": "application/x-www-form-urlencoded" }
-                }).then(function(res) {
-                    that.options = res.data.data.data;
-                });
+        transformRequest: [
+          function(data) {
+            let ret = "";
+            for (let it in data) {
+              ret +=
+                encodeURIComponent(it) +
+                "=" +
+                encodeURIComponent(data[it]) +
+                "&";
+            }
+            return ret;
+          }
+        ],
+        headers: { "Content-Type": "application/x-www-form-urlencoded" }
+      }).then(function(res) {
+        if (res.data.code == "0") {
+          that.dialogFormVisible = false;
+          that.show();
         }
+      });
+    },
+    editChild(e) {
+      // 修改  弹框
+      this.p_id = e.currentTarget.getAttribute("data_id");
+      let that = this;
+      this.$axios({
+        url: `api/bqs/backend/web/index.php/oauth/update?id=${this.p_id}`,
+        method: "get",
+        data: {},
+        transformRequest: [
+          function(data) {
+            let ret = "";
+            for (let it in data) {
+              ret +=
+                encodeURIComponent(it) +
+                "=" +
+                encodeURIComponent(data[it]) +
+                "&";
+            }
+            return ret;
+          }
+        ],
+        headers: { "Content-Type": "application/x-www-form-urlencoded" }
+      }).then(function(res) {
+        if (res.data.code == "0") {
+          that.form.name = res.data.data.name;
+          if (res.data.data.type == 0) {
+            that.form.region = "动作权限";
+          } else {
+            that.form.region = "菜单权限";
+          }
+          that.form.con = res.data.data.controller;
+          that.form.act = res.data.data.action;
+          that.dialogFormVisibleEdit = true;
+
+          // that.form.icon = res.data.data.icon;
+        }
+      });
+    },
+    editOrder() {
+      let that = this;
+      this.$axios({
+        url: `api/bqs/backend/web/index.php/oauth/update`,
+        method: "post",
+        data: {
+          action: this.form.act,
+          controller: this.form.con,
+          name: this.form.name,
+          type: this.form.region,
+          id: this.p_id
+        },
+        transformRequest: [
+          function(data) {
+            let ret = "";
+            for (let it in data) {
+              ret +=
+                encodeURIComponent(it) +
+                "=" +
+                encodeURIComponent(data[it]) +
+                "&";
+            }
+            return ret;
+          }
+        ],
+        headers: { "Content-Type": "application/x-www-form-urlencoded" }
+      }).then(function(res) {
+          
+        if (res.data.code == "0") {
+            that.dialogFormVisibleEdit = false;
+            that.show();
+        }
+      });
+    },
+    delChild(e) {
+      // 删除 弹框  需要二次确认
+      this.p_id = e.currentTarget.getAttribute("data_id");
+      this.dialogVisible = true;
+    },
+    delOrder() {
+      // 删除  提交
+      let that = this;
+      this.$axios({
+        url: "api/bqs/backend/web/index.php/oauth/delete",
+        method: "post",
+        data: {
+          p_path: this.p_id
+        },
+        transformRequest: [
+          function(data) {
+            let ret = "";
+            for (let it in data) {
+              ret +=
+                encodeURIComponent(it) +
+                "=" +
+                encodeURIComponent(data[it]) +
+                "&";
+            }
+            return ret;
+          }
+        ],
+        headers: { "Content-Type": "application/x-www-form-urlencoded" }
+      }).then(function(res) {
+        if (res.data.code == "0") {
+          that.dialogVisible = false;
+          that.show();
+        }
+      });
     }
+  }
+};
 </script>
 <style scoped>
-td,th{border:solid #ccc; border-width:0px 1px 1px 0px; padding:10px 0px;text-align: center;}
-table{border:solid #ccc; border-width:1px 0px 0px 1px;border-collapse: collapse;}
+td,
+th {
+  border: solid #ccc;
+  border-width: 0px 1px 1px 0px;
+  padding: 10px 0px;
+  text-align: center;
+}
+tr td:nth-child(2) {
+  text-align: left;
+  padding: 0 0 0 40px;
+}
+table {
+  border: solid #ccc;
+  border-width: 1px 0px 0px 1px;
+  border-collapse: collapse;
+}
+.divBut {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
 </style>
--- src/components/page/VueTable.vue
+++ src/components/page/VueTable.vue
@@ -1,95 +1,148 @@
 <template>
-    <div class="table">
-        <div class="crumbs">
-            <el-breadcrumb separator="/">
-                <el-breadcrumb-item><i class="el-icon-menu"></i> 表格</el-breadcrumb-item>
-                <el-breadcrumb-item>Vue表格组件</el-breadcrumb-item>
-            </el-breadcrumb>
+    <div>
+      <div class="divBut">
+            <el-row>
+                <el-button type="primary" round @click="add()">添加</el-button>
+                <el-button type="primary" round @click="">修改</el-button>
+                <el-button type="primary" round @click="">删除</el-button>
+            </el-row>
+            <div style="margin:0 60px 0 0;">
+                总条数：<span>{{count}}</span>条
+            </div>
         </div>
-        <div class="plugins-tips">
-            vue-datasource：一个用于动态创建表格的vue.js服务端组件。
-            访问地址：<a href="https://github.com/coderdiaz/vue-datasource" target="_blank">vue-datasource</a>
+            <!-- 表格 -->
+         <table style="width:100%;border:1px solid #ccc;margin:10px 0 0 0;">
+          <thead>
+            <th><input type="checkbox"></th>
+            <th>序号</th>
+            <th>角色</th>
+            <th>添加时间</th>
+            <th>操作</th>
+          </thead>
+          <tbody>
+            <template v-for="(item,index) in options">
+                    <tr>
+                        <td><input type="checkbox" :data_id="item.r_id"></td>
+                        <td>{{Number(index)+1}}</td>
+                        <td>{{item.name}}</td>
+                        <td>{{item.addtime | formatDate}}</td>
+                        <td>
+                            <el-button type="danger" size="small" round :data_id="item.p_path" @click="delChild($event)">分配权限</el-button>
+                        </td>
+                    </tr>        
+            </template>    
+          </tbody>
+        </table>
+ <!-- 添加   弹框 -->
+        <el-dialog title="添加角色" :visible.sync="dialogFormVisible">
+        <el-form :model="form">
+            <el-form-item label="角色名称" :label-width="formLabelWidth">
+            <el-input v-model="form.name" auto-complete="off"></el-input>
+            </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+            <el-button @click="dialogFormVisible = false">取 消</el-button>
+            <el-button type="primary" @click="addOrder()">确 定</el-button>
         </div>
-        <datasource language="en" :table-data="getData" :columns="columns" :pagination="information.pagination"
-                :actions="actions"
-                v-on:change="changePage"
-                v-on:searching="onSearch"></datasource>
+        </el-dialog>
     </div>
 </template>
 
 <script>
-    import axios from 'axios';
-    import Datasource from 'vue-datasource';
-    export default {
-        data: function(){
-            const self = this;
-            return {
-                url: './static/datasource.json',
-                information: {
-                    pagination:{},
-                    data:[]
-                },
-                columns: [
-                    {
-                        name: 'Id',
-                        key: 'id',
-                    },
-                    {
-                        name: 'Name',
-                        key: 'name',
-                    },
-                    {
-                        name: 'email',
-                        key: 'email',
-                    },
-                    {
-                        name: 'ip',
-                        key: 'ip',
-                    }
-                ],
-                actions: [
-                    {
-                        text: 'Click',
-                        class: 'btn-primary',
-                        event(e, row) {
-                            self.$message('选中的行数： ' + row.row.id);
-                        }
-                    }
-                ],
-                query:''
+import { formatDate } from "./../../js/data";
+export default {
+  data: function() {
+    const self = this;
+    return {
+      options: [],
+      count: "",
+      dialogFormVisible: false,
+      formLabelWidth: "120px",
+      form: {
+        name: ""
+      }
+    };
+  },
+  components: {},
+  created() {
+    this.show(); // 刚进来的时候  请求数据
+  },
+  methods: {
+    show() {
+      let that = this;
+      this.$axios({
+        url: `api/bqs/backend/web/index.php/role/list`,
+        method: "get",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" }
+      }).then(function(res) {
+        that.options = res.data.data.data;
+        that.count = res.data.data.count;
+      });
+    },
+    add() {
+      // 添加 弹框
+      this.dialogFormVisible = true;
+    },
+    addOrder() {
+      // 添加  提交
+      let that = this;
+      this.$axios({
+        url: "api/bqs/backend/web/index.php/role/add",
+        method: "post",
+        data: {
+            name: this.form.name
+        },
+        transformRequest: [
+          function(data) {
+            let ret = "";
+            for (let it in data) {
+              ret +=
+                encodeURIComponent(it) +
+                "=" +
+                encodeURIComponent(data[it]) +
+                "&";
             }
-        },
-        components: {
-            Datasource
-        },
-        methods: {
-            changePage(values) {
-                this.information.pagination.per_page = values.perpage;
-                this.information.data = this.information.data;
-            },
-            onSearch(searchQuery) {
-                this.query = searchQuery;
-            }
-        },
-        computed:{
-            getData(){
-                const self = this;
-                return self.information.data.filter(function (d) {
-                    if(d.name.indexOf(self.query) > -1){
-                        return d;
-                    }
-                })
-            }
-        },
-        beforeMount(){
-            if(process.env.NODE_ENV === 'development'){
-                this.url = '/ms/table/source';
-            };
-            axios.get(this.url).then( (res) => {
-                this.information = res.data;
-            })
-        }
+            return ret;
+          }
+        ],
+        headers: { "Content-Type": "application/x-www-form-urlencoded" }
+      }).then(function(res) {
+       if(res.data.code == 0){
+           that.dialogFormVisible = false;
+            that.show();
+       }
+      });
     }
+  },
+  computed: {},
+  beforeMount() {},
+  filters: {
+    formatDate(time) {
+      var date = new Date(time * 1000);
+      return formatDate(date, "yyyy-MM-dd hh:mm:ss");
+    }
+  }
+};
 </script>
 
-<style src="../../../static/css/datasource.css"></style>
+<style scoped>
+td,
+th {
+  border: solid #ccc;
+  border-width: 0px 1px 1px 0px;
+  padding: 10px 0px;
+  text-align: center;
+  width: 20%;
+}
+
+table {
+  border: solid #ccc;
+  border-width: 1px 0px 0px 1px;
+  border-collapse: collapse;
+}
+.divBut {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
